558546f1a16b47146a7f0bc19baff1d70da37d3b
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-06-25 13:12:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+sudo apt-get install libbluetooth-dev
 sudo apt install libbluetooth3
 pip3 install pybluez
+python3 -m pip install pybluez
 dudo apt-get --fix-broken install
 sudo apt install bettercap
